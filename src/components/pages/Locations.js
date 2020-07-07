089a1bd5972f2936c5ddcc6d003d626ae3d9b299
James B. Coronel <james.b.coronel@gmail.com>
2020-07-06 21:24:47 -0700

    hopefully it works this time

--- src/components/pages/Locations.js
+++ src/components/pages/Locations.js
@@ -3,9 +3,12 @@ import Map from '../other/GoogleMap';
 
 function Locations() {
     useEffect(()=> {
+        let header = document.querySelector('#header');
         let wrapper = document.querySelector('.wrapper');
         wrapper.classList.add('location_active');
+        header.style.position = 'relative';
         return function () {
+            header.style.position = 'fixed';
             wrapper.classList.remove('location_active');
         }
     })
