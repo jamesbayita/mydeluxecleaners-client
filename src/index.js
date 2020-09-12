c570cb00e40bfa8d1858f05df843c18f13a8daf1
James B. Coronel <james.b.coronel@gmail.com>
2020-09-11 19:47:20 -0700

    v1.0.0

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import './css/index.css';
 import App from './components/App';
 import {BrowserRouter as Router} from 'react-router-dom';
 import ScrollToTop from './components/helpers/scrollToTop';
-import ReactGA from 'react-ga';
-
-ReactGA.initialize('UA-177778106-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
